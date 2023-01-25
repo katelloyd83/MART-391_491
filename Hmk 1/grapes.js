20ad90bc47a77353eb11ec584016fa8cdcd0ac8f
Kate Lloyd <112956534+katelloyd83@users.noreply.github.com>
2023-01-24 19:29:18 -0700

    hmk1
    grapes

---
+++ Hmk 1/grapes.js
@@ -0,0 +1,67 @@
+function setup() {
+    createCanvas(500, 600);
+    background(0);
+  }
+  
+  function draw() {
+    background(200, 200, 200);
+    fill(0, 0, 0);
+    strokeWeight(3);
+  
+    fill(94, 90, 128);
+    circle(300, 200, 100);
+  
+    fill(94, 90, 128);
+    circle(225, 200, 100);
+  
+    fill(94, 90, 128);
+    circle(280, 125, 100);
+  
+    fill(94, 90, 128);
+    circle(275, 125, 100);
+  
+    fill(94, 90, 128);
+    circle(240, 100, 111);
+  
+    fill(94, 90, 128);
+    circle(195, 170, 70);
+  
+    fill(94, 90, 128);
+    circle(195, 245, 80);
+  
+    fill(94, 90, 128);
+    circle(265, 150, 90);
+  
+    fill(94, 90, 128);
+    circle(170, 245, 50);
+  
+    fill(94, 90, 128);
+    circle(265, 245, 60);
+  
+    fill(94, 90, 128);
+    circle(170, 100, 80);
+  
+    fill(94, 90, 128);
+    circle(170, 150, 110);
+  
+    fill(94, 90, 128);
+    circle(125, 222, 99);
+  
+    fill(94, 90, 128);
+    circle(199, 100, 80);
+  
+    fill(94, 90, 128);
+    circle(120, 190, 80);
+  
+    fill(78, 53, 36);
+    translate(width / 2, height / 2);
+    rotate(PI / -5.0);
+    rect(130, -100, 125, 20);
+  
+    fill(34, 139, 34);
+    ellipse(200, -50, 20, 80);
+  
+    fill(34, 139, 34);
+    ellipse(180, -130, 20, 80);
+  }
+  
